1145720e98a1801e2635fe3044308104ecb8972b
Benoit Chauvet <bchauvet@nx-domain.lan>
2021-03-09 14:32:42 +0100

    navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { MoviesComponent } from './movies/movies.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'heroes', component : HeroesComponent},
+  { path : 'films', component : MoviesComponent},
+  { path : '', redirectTo: '/heroes', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
